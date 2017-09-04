6178ee13c0539ed0ffe97ec6444027a97ebb6f98
eagleLiu82 <25404210@qq.com>
2017-09-04 16:25:17 +0800

    modify use template

--- mysite/votes/urls.py
+++ mysite/votes/urls.py
@@ -8,6 +8,13 @@ Created on Sat Sep  2 21:25:32 2017
 from django.conf.urls import url
 from . import views
 
+app_name = 'votes'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # ex: /votes/5/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # ex: /votes/5/results/
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    # ex: /votes/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
