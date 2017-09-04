12fef3970a4a11852587e3bb0579a965e8cbb7ae
eagleLiu82 <25404210@qq.com>
2017-09-04 17:03:12 +0800

    add test

--- mysite/votes/views.py
+++ mysite/votes/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .models import Question,Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -12,7 +13,8 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        
 
 
 class DetailView(generic.DetailView):
